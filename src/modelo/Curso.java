bf93054fe1f0e3e4a60914bb4665be3a83fd62ee
Jonathan_Leder <jonyleder@gmail.com>
2023-03-13 09:44:11 -0300

    se agrego comportamiento a los cursos y metodos necesarios a la clase participante

--- src/modelo/Curso.java
+++ src/modelo/Curso.java
@@ -29,15 +29,27 @@ public class Curso {// El objetivo es cumplir con las consignas asignadas por en
 	public void inscribirParticipante(Participante unParticipante) {
 
 		/*
-		 * antes que nada evaluar si ya esta inscripto 1- Evaluar si es el primer dia si
-		 * es el primer dia sumar 10 puntos al participante(crear metodo para ello,
-		 * metodo sumar puntos) 2- Evaluar si esta dentro del periodo 2.1 si no esta
-		 * dentro del periodo por pantalla se le informa que debe mandar una nota 2.2
-		 * caso contrario se lo inscribe en el curso
+		 * antes que nada evaluar si ya esta inscripto(si aparece en la lista) 1-
+		 * Evaluar si es el primer dia si es el primer dia sumar 10 puntos al
+		 * participante(crear metodo para ello, metodo sumar puntos) 2- Evaluar si esta
+		 * dentro del periodo 2.1- si no esta dentro del periodo por pantalla se le
+		 * informa que debe mandar una nota
 		 */
+		if (!participantes.contains(unParticipante)) {
+			if (LocalDate.now().equals(iInscripcion)) {
+				unParticipante.sumarPuntos(10);
+				participantes.add(unParticipante);
+			} else if (sePuedeInscribir()) {
+				participantes.add(unParticipante);
+			} else
+				System.out.println("   Para inscribirse fuera de termino, mande una nota. Muchas gracias");
+		}
+
 	}
 
-	private void sumarPuntos(int puntos) {
-
+	private boolean sePuedeInscribir() {
+		return LocalDate.now().getDayOfYear() > iInscripcion.getDayOfYear()
+				&& LocalDate.now().getDayOfYear() <= fInscripcion.getDayOfYear();
 	}
+
 }
--- src/modelo/Participante.java
+++ src/modelo/Participante.java
@@ -6,6 +6,22 @@ public class Participante {
 	int dni;
 	int puntos;
 
+	// --- Constructor/es ---
+
+	public Participante(String nombre, int dni) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.puntos = 0;
+	}
+
 	// --- Metodos ---
+
+	public void sumarPuntos(int puntos) {
+		this.puntos += puntos;
+	}
+
+	public int getPuntaje() {
+		return this.puntos;
+	}
 
 }
